b65487294610b46514777ca80a90ea746af007f2
gajerakashyap26-lab <gajerakashyap26@gmail.com>
2025-12-31 09:12:03 +0530

    Add files via upload

---
+++ assignment operator.py
@@ -0,0 +1,41 @@
+#Assigment Operator
+x=10
+print(10)
+
+#urnary minus+
+a=5
+print(-a)
+
+#relational
+p=10
+q=5
+print(p>q)
+print(p==q)
+
+#boolean
+is_pass = True
+print(is_pass)
+
+#logical
+x=True
+y=False
+print(x and y)
+print(x or y)
+print( not x)
+
+#bitwise
+a=5
+b=3
+print(a & b)
+print(a | b)
+
+#membership
+numbers = [1,2,3,4,5,6]
+print(2 in numbers)
+print(5 not in numbers)
+
+#identity
+x=10 
+y=10
+print(x is y)
+print(x is not y)
