7e8f496ca33173eedb5147c201433c5d95c43006
gajerakashyap26-lab <gajerakashyap26@gmail.com>
2026-01-19 10:29:49 +0530

    Add files via upload

---
+++ Types of operators.py
@@ -0,0 +1,41 @@
+#Assigment operator
+x=10
+print(x)
+
+#urnary minus+
+a=5
+print(-a)
+
+#relational operator
+p=10
+q=5
+print(p>q)
+print(p==q)
+
+#logical operators
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+
+#boolean operators
+is_pass= True
+print(is_pass)
+
+#bitwise operators
+a=5
+b=3
+print(a & b)
+print(a | b)
+
+#membership operators
+numbers=[1,2,3,4,5,6]
+print(2 in numbers)
+print(5 not in numbers)
+
+#identity operators
+x=10
+y=10
+print(x is y)
+print(x is not y)
